f520b7ea890ffbe06ad0d9d015a217e00fe3f7c6
Simon Cropp <simon.cropp@gmail.com>
2026-01-03 15:01:42 +1100

    refs

--- src/PackageShader/Signing/StrongNameKey.cs
+++ src/PackageShader/Signing/StrongNameKey.cs
@@ -264,10 +264,7 @@ public sealed class StrongNameKey
 
     static byte[] ComputePublicKeyToken(byte[] publicKey)
     {
-        using var sha1 = SHA1.Create();
-#pragma warning disable CA1850
-        var hash = sha1.ComputeHash(publicKey);
-#pragma warning restore CA1850
+        var hash = SHA1.HashData(publicKey);
 
         // Take last 8 bytes, reversed
         var token = new byte[8];
